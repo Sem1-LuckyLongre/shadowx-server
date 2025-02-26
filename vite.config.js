a56c4f9659964c1893544a4cf640333f696c738b
Sem 1 <sem1luckylongre@gmail.com>
2025-02-26 12:26:49 +0530

    vite config host allowed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host:true,
+    port: 5173,
+    allowedHosts:['h2dmtg-5173.csb.app']
+  }
 })
