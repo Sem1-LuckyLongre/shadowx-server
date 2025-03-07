c21ae63f62eaa03cb7f416bf6a5c628d0535f5ca
Sem 1 <sem1luckylongre@gmail.com>
2025-03-07 15:45:22 +0530

    aaa

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -2,11 +2,11 @@ import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
 import { useTheme } from "../context/ThemeContext";
 
-const AppLayout = ({Login}) => {
+const AppLayout = ({ Login }) => {
   const { isDarkMode } = useTheme();
-
+  console.log("corps allowed");
   return (
-    <div className={`min-h-screen ${isDarkMode ? 'dark' : ''}`}>
+    <div className={`min-h-screen ${isDarkMode ? "dark" : ""}`}>
       <div className="flex flex-col min-h-screen bg-[rgb(var(--background))]">
         <NavBar Login={Login} />
         <main className="flex-grow px-4 py-8">
