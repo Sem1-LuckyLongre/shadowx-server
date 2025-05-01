45212120639daa040f4a3f5f8c21bae22d656ffa
Sem 1 <sem1luckylongre@gmail.com>
2025-05-01 20:13:08 +0530

    feat: display user's initial in ChangePassword component

--- src/components/ChangePassword.jsx
+++ src/components/ChangePassword.jsx
@@ -5,7 +5,7 @@ import { useTheme } from "../context/ThemeContext";
 import { Loader } from "./Loader";
 
 export const ChangePassword = () => {
-  const { autherizedToken, URI, userAuthentication } = useTheme();
+  const { autherizedToken, URI, userAuthentication, user } = useTheme();
 
   const [isLoading, setIsLoading] = useState(false);
 
@@ -172,7 +172,9 @@ export const ChangePassword = () => {
         <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center gap-4">
           <div className="flex items-center space-x-4">
             <div className="w-10 h-10 rounded-full bg-gradient-to-r from-primary-500 to-blue-400 flex items-center justify-center">
-              <span className="font-bold text-white">U</span>
+              <span className="font-bold text-white">
+                {user.name.trim().charAt(0).toUpperCase() || "U"}
+              </span>
             </div>
             <h1 className="text-xl font-bold bg-gradient-to-r from-primary-500 to-blue-400 bg-clip-text text-transparent">
               Change Password
